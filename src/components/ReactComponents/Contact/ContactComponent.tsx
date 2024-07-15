492366cbea2b17370e2701fbc9bf261d5d2f41e8
Juan Pablo Gonzalez <jupago14@gmail.com>
2024-07-15 14:45:07 -0500

    fixing urls

--- src/components/ReactComponents/Contact/ContactComponent.tsx
+++ src/components/ReactComponents/Contact/ContactComponent.tsx
@@ -18,8 +18,8 @@ const ContactComponent = ({currentLang}: ContactProps) => {
     </p>
     <div className={styles.contactInfo}>
         <h1 className={styles.title}>{t("contact_me.contact")}</h1>
-        <a className={styles.contactLink} href="https://www.linkedin.com/in/jpabgonzalez" target="_blank"><img className={styles.icons} src="/git.svg" alt="social_media"/>GitHub</a>
-        <a className={styles.contactLink} href="https://github.com/jap102321" target="_blank"><img className={styles.icons} src="/linkedin.svg" alt="social_media"/>LinkedIn</a>
+        <a className={styles.contactLink} href="https://www.linkedin.com/in/jpabgonzalez" target="_blank"><img className={styles.icons} src="/linkedin.svg" alt="social_media"/>LinkedIn</a>
+        <a className={styles.contactLink} href="https://github.com/jap102321" target="_blank"><img className={styles.icons} src="/git.svg" alt="social_media"/>GitHub</a>
         <a className={styles.contactLink} href="mailto:jupago14@gmail.com" target="_blank"><img className={styles.icons} src="/mail.svg" alt="social_media"/> Email</a>
     </div>
   </div>;
